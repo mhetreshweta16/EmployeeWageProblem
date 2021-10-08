c243f3ec45787954a7e9a79d4e03caab0df88c37
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-08 16:31:43 +0530

    [adding new code to UC3]

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,13 +6,14 @@ namespace EmployeeWageProblem
     {
         public const int EmpRatePerHrs= 20;
         public const int IS_FullTime = 1;
+        public const int IS_PartTime = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
             int empHrs= 0;
 
             Random r = new Random();
-            int empInput = r.Next(0, 2);
+            int empInput = r.Next(0, 3);
             Console.WriteLine(empInput);
             if (IS_FullTime == empInput)
             {
@@ -20,8 +21,14 @@ namespace EmployeeWageProblem
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
             }
+            else if (IS_PartTime == empInput)
+            {
+
+                Console.WriteLine("Employee is Present");
+                empHrs = 4;
+            }
             else {
-                
+
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
