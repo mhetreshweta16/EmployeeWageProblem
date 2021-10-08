22d4b9b8a67b2013889e2d387514ae7527a42628
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-08 15:47:56 +0530

    Adding files to UC1

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -3,10 +3,23 @@
 namespace EmployeeWageProblem
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
+            int IS_Present = 1;
+            Random r = new Random();
+            int empInput = r.Next(0, 2);
+            Console.WriteLine(empInput);
+            if (IS_Present == empInput)
+            {
+                
+                Console.WriteLine("Employee is Present");
+            }
+            else {
+                
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
