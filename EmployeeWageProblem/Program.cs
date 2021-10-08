4ac19955aa15c64464252547b9ddf4fa80eb5134
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-08 16:18:45 +0530

    adding new code to UC2

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -3,23 +3,34 @@
 namespace EmployeeWageProblem
 {
     class Program
-    { 
+    {
+        public const int EmpRatePerHrs= 20;
+        public const int IS_FullTime = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
-            int IS_Present = 1;
+            int empHrs= 0;
+
             Random r = new Random();
             int empInput = r.Next(0, 2);
             Console.WriteLine(empInput);
-            if (IS_Present == empInput)
+            if (IS_FullTime == empInput)
             {
-                
+
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else {
                 
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            int empWage = EmpRatePerHrs * empHrs;
+            Console.WriteLine("the Employee age is {0}", empWage);
+            Console.ReadLine();
+
+
+
         }
     }
 }
