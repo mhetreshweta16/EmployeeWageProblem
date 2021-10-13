4bda61367bea204391f54340f57d433ea519c1fb
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-13 11:59:21 +0530

    [compute empwage code using class in UC7 Branch]

---
+++ EmployeeWageProblem/EmpWageCompute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    class EmpWageCompute
+    {
+        public int checkEmpTime(int empInput)
+        {
+
+            const int IS_FullTime = 1;
+            const int IS_PartTime = 2;
+            int empHrs = 0;
+
+            switch (empInput)
+            {
+                case IS_FullTime:
+
+                    return empHrs = 8;
+                case IS_PartTime:
+
+                    return empHrs = 4;
+
+                default:
+
+                    return empHrs = 0;
+            }
+
+        }
+
+
+
+
+
+
+        public int empWageCompute(int EmpRatePerHrs, int Num_Of_Working_Days, int WorkingHrs)
+        {
+
+            int totalEmpHrs = 0;
+            int totalEmpWage = 0;
+            int day = 1;
+
+            while (day <= Num_Of_Working_Days && totalEmpHrs <= WorkingHrs)
+            {
+                Random r = new Random();
+                int empInput = r.Next(0, 3);
+                //Console.WriteLine(empInput);
+                int empHrs = checkEmpTime(empInput);
+
+                int empWage = EmpRatePerHrs * empHrs;
+                totalEmpWage += empWage;
+                Console.WriteLine("daily Employee Wage for {0} day {1}", day, empWage);
+                totalEmpHrs += empHrs;
+                day++;
+            }
+
+            Console.WriteLine("total employee wage for {0} day {1} Max Work Hrs{2} ", day - 1, totalEmpWage, totalEmpHrs);
+            Console.ReadLine();
+            return 0;
+        }
+
+    }
+}
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,51 +4,20 @@ namespace EmployeeWageProblem
 {
     class Program
     {
-        public const int EmpRatePerHrs= 20;
-        public const int IS_FullTime = 1;
-        public const int IS_PartTime = 2;
-        public const int Num_Of_Working_Days = 20;
-        public const int WorkingHrs = 50;
+        //public const int EmpRatePerHrs= 20;
+       
+       // public const int Num_Of_Working_Days = 20;
+        //public const int WorkingHrs = 60;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
-            int empHrs = 0, totalEmpHrs = 0;
-            int totalEmpWage = 0;
-            int day = 1;
-            while( day <= Num_Of_Working_Days && totalEmpHrs <= WorkingHrs )
-            {
-                Random r = new Random();
-                int empInput = r.Next(0, 3);
-                //Console.WriteLine(empInput);
-                switch (empInput)
-                {
+            EmpWageCompute emp = new EmpWageCompute();
+            emp.empWageCompute(20, 20, 60);
 
-                    case IS_FullTime:
-                        //Console.WriteLine("Employee is Present");
-                        empHrs = 8;
-                        break;
 
-                    case IS_PartTime:
-                        //Console.WriteLine("Employee is Present");
-                        empHrs = 4;
-                        break;
 
-                    default:
-                        //Console.WriteLine("Employee is Absent");
-                        empHrs = 0;
-                        break;
-                }
-                
-                int empWage = EmpRatePerHrs * empHrs;
-                totalEmpWage += empWage;
-                Console.WriteLine("daily Employee Wage for {0} day {1}", day, empWage);
-                totalEmpHrs += empHrs;
-                day++;
-            }
-
-            Console.WriteLine("total employee wage for {0} day {1} Max Work Hrs{2} ",day-1, totalEmpWage,totalEmpHrs);
-            Console.ReadLine();
 
         }
+    
     }
 }
